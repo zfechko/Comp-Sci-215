11964d7dd506473df46357952dc12ce4ad1443c3
Zachary Fechko <zachary.fechko@wsu.edu>
2022-01-13 11:16:14 -0800

    started practicing python

---
+++ Self Practice/practice.py
@@ -0,0 +1,17 @@
+# practicing defining a function by making a simple add function
+def add(x, y):
+    print(x + y)
+
+add(2.5, 3.8)
+
+# practicing defining and accessing a list
+practice_list = ["zach", "elliot", "josh"]
+practice_list2 = ["fechko", "kimsey", "maloy"]
+
+
+practice_list.append("noah") # funtions like a push_back for C++ vectors
+practice_list2.append("frachon")
+
+practice_list.extend(practice_list2) # concatenates two lists by appending practice_list2 to the end of practice_list
+
+
