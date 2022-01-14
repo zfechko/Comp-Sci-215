ac704df0183c9d53bcea8af2e95d045e9a25d883
Zachary Fechko <zachary.fechko@wsu.edu>
2022-01-14 09:57:59 -0800

    Added the materials for the 1st micro assignment

---
+++ Micro Assignments/MA1/hello_world.py
@@ -0,0 +1,6 @@
+"""
+ this is the same file that I created in the terminal, if I was supposed to make another hello_world.py please let me know 
+ My IDE of choice is Visual Studio Code because I already had it installed, if I am required to use a different IDE, please let me know
+"""
+
+print("Hello World")
