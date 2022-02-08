a6258c1fc5d9d0b3070e40f254a38364139f31e0
Zachary Fechko <zachary.fechko@wsu.edu>
2022-02-08 09:37:57 -0800

    added concept from monday 2/7

---
+++ In Class Activities/Iterative_vs_Recursive.py
@@ -0,0 +1,35 @@
+
+def iterative_binary_search(alist, target):
+    """
+    Iterative implementation of binary search
+    Time complexity: O(log(n))
+    """
+    start = 0
+    end = len(alist) - 1
+    mid = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+        if alist[mid] < target:
+            low = mid + 1
+        elif alist[mid] > target:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+
+
+def recursive_binary_search(alist, target, start, end):
+    """
+    Recursive implementation of binary search
+    Time Complexity: O(log2(n))
+    """
+    if start > end:
+        return -1
+    mid = (start + end) / 2
+    if alist[mid] == target:
+        return mid
+    elif alist[mid] < target:
+        return recursive_binary_search(alist, target, mid + 1, end)
+    else:
+        return recursive_binary_search(alist, target, start, mid - 1)
