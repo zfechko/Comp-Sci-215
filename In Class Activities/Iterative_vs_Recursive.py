780606a4eec6b3bccc0737241e8c08f858b27a94
Zachary Fechko <zachary.fechko@wsu.edu>
2022-02-09 11:15:38 -0800

    fixed a small bug

--- In Class Activities/Iterative_vs_Recursive.py
+++ In Class Activities/Iterative_vs_Recursive.py
@@ -26,10 +26,15 @@ def recursive_binary_search(alist, target, start, end):
     """
     if start > end:
         return -1
-    mid = (start + end) / 2
+    mid = (start + end) // 2
     if alist[mid] == target:
         return mid
     elif alist[mid] < target:
         return recursive_binary_search(alist, target, mid + 1, end)
     else:
         return recursive_binary_search(alist, target, start, mid - 1)
+
+    
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(recursive_binary_search(array, 6, 0, len(array) - 1))
