e96e36f901ef1535bbea6115d575cec5c3217db8
Zachary Fechko <zachary.fechko@wsu.edu>
2022-02-14 10:32:58 -0800

    updated in class code

--- In Class Activities/Sorting Algorithms.py
+++ In Class Activities/Sorting Algorithms.py
@@ -1,6 +1,12 @@
+import random
+
+arr = []
+
 def selection_sort(array):
     """
-    Time Complexity: O(n^2)
+    Performs selection sort on a list/array
+    Worst Case Time Complexity: O(n^2)
+    Best Case Time Complexity: Ω(n^2)
     Space Complexity: O(1)
     """
     for i in range (0, len(array)):
@@ -12,4 +18,55 @@ def selection_sort(array):
             temp = array[i]
             array[i] = array[smallest_index]
             array[smallest_index] = temp
+
+def bubble_sort(array):
+    """
+    Performs bubble sort on a list/array
+    Worst Case Time Complexity: O(n^2)
+    Best Case Time Complexity: Ω(n^2)
+    Space Complexity: O(1)     
+    """
+    for i in range(0, len(array) - 1):
+        for j in range(0, len(array) - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+
+def insertion_sort(array):
+    """
+    Performs insertion sort on a list/array
+    Worst Case Time Complexity: O(n^2)
+    Best Case Time Complexity: Ω(n)
+    Space Complexity:
+    """
+    for i in range(1, len(array)):
+  
+        key = array[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+                array[j+1] = array[j]
+                j -= 1
+        array[j+1] = key
+
+def shell_sort(array):
+    """
+    Performs shell sort on a list/array
+    Worst Case Time Complexity: O(n(log(n))^2)
+    Best Case Time Complexity: Ω(n log(n))
+    Space Complexity: 
+    """
+    gap = len(array) // 2
+    
+    while (gap >= 1):
+        for k in range(0, gap):
+            for i in range(k + gap, len(array), gap):
+                j = i
+
+                while(j - gap) >= 0 and array[j - gap] > array[j]:
+                    temp = array[j]
+                    array[j] = array[j - gap]
+                    array[j - gap] = temp
+                    j = j - gap
+            gap = gap // 2
 
